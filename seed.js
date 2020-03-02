36e1021ac0ac8e5d319f1d46aa2621ca2b61a2df
Erick <ecanals07@gmail.com>
2020-03-02 18:21:32 -0500

    finished seeding users table in database

--- seed.js
+++ seed.js
@@ -2,6 +2,7 @@
 const Users = require('./server/db/models/user')
 const Products = require('./server/db/models/product')
 const Orders = require('./server/db/models/order')
+const db = require('./server/db/db')
 const faker = require('faker')
 
 // first name faker.fake('{{name.firstName}}')
@@ -11,49 +12,65 @@ const faker = require('faker')
 // address faker.fake('{{address.streetAddress}}')
 // admin faker.fake('{{random.boolean}}')
 
-// const seed = async () => {
-//   try {
-//     await db.sync({ force: true });
+const instanceCount = 100
+const users = []
 
-//     await Promise.all(
-//       robots.map(robot => {
-//         return Robot.create(robot);
-//       })
-//     );
+for (let i = 0; i < instanceCount; i++) {
+  const randomUser = {
+    email: faker.fake('{{internet.email}}'),
+    password: faker.fake('{{internet.password}}'),
+    address: faker.fake('{{address.streetAddress}}'),
+    admin: faker.fake('{{random.boolean}}'),
+    lastName: faker.fake('{{name.lastName}}'),
+    firstName: faker.fake('{{name.firstName}}')
+  }
 
-//     await Promise.all(
-//       projects.map(project => {
-//         return Project.create(project);
-//       })
-//     );
+  users.push(randomUser)
+}
 
-//     const robotsArr = await Robot.findAll();
-//     const projectsArr = await Project.findAll();
+const seed = async () => {
+  try {
+    await db.sync({force: true})
 
-//     for (let i = 0; i < robots.length; i++) {
-//       await robotsArr[i].setProjects(projectsArr);
-//       await projectsArr[i].setRobots(robotsArr);
-//     }
+    await Promise.all(
+      users.map(user => {
+        return Users.create(user)
+      })
+    )
 
-//     // seed your database here!
-//   } catch (err) {
-//     console.log(red(err));
-//   }
-// };
+    // await Promise.all(
+    //   projects.map(project => {
+    //     return Project.create(project)
+    //   })
+    // )
 
-// module.exports = seed
-// // If this module is being required from another module, then we just export the
-// // function, to be used as necessary. But it will run right away if the module
-// // is executed directly (e.g. `node seed.js` or `npm run seed`)
-// if (require.main === module) {
-//   seed()
-//     .then(() => {
-//       console.log(green('Seeding success!'))
-//       db.close()
-//     })
-//     .catch(err => {
-//       console.error(red('Oh noes! Something went wrong!'))
-//       console.error(err)
-//       db.close()
-//     })
-// }
+    // const robotsArr = await Robot.findAll()
+    // const projectsArr = await Project.findAll()
+
+    // for (let i = 0; i < robots.length; i++) {
+    //   await robotsArr[i].setProjects(projectsArr)
+    //   await projectsArr[i].setRobots(robotsArr)
+    // }
+
+    // seed your database here!
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+module.exports = seed
+// If this module is being required from another module, then we just export the
+// function, to be used as necessary. But it will run right away if the module
+// is executed directly (e.g. `node seed.js` or `npm run seed`)
+if (require.main === module) {
+  seed()
+    .then(() => {
+      console.log('Seeding success!')
+      db.close()
+    })
+    .catch(err => {
+      console.error('Oh noes! Something went wrong!')
+      console.error(err)
+      db.close()
+    })
+}
