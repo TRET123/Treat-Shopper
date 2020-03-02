bdc1984cfbb69b8b39a7ad980bc05fd1c877c935
Erick <ecanals07@gmail.com>
2020-03-02 17:12:26 -0500

    figured out what methods are needed to generate fake users

--- seed.js
+++ seed.js
@@ -1,5 +1,59 @@
 // require database and User and Products model
+const Users = require('./server/db/models/user')
+const Products = require('./server/db/models/product')
+const Orders = require('./server/db/models/order')
 const faker = require('faker')
 
-console.log('here:')
-console.log(faker.fake('{{name.firstName}} {{name.lastName}}'))
+// first name faker.fake('{{name.firstName}}')
+// last name faker.fake('{{name.lastName}}')
+// email faker.fake('{{internet.email}}')
+// password faker.fake('{{internet.password}}'
+// address faker.fake('{{address.streetAddress}}')
+// admin faker.fake('{{random.boolean}}')
+
+// const seed = async () => {
+//   try {
+//     await db.sync({ force: true });
+
+//     await Promise.all(
+//       robots.map(robot => {
+//         return Robot.create(robot);
+//       })
+//     );
+
+//     await Promise.all(
+//       projects.map(project => {
+//         return Project.create(project);
+//       })
+//     );
+
+//     const robotsArr = await Robot.findAll();
+//     const projectsArr = await Project.findAll();
+
+//     for (let i = 0; i < robots.length; i++) {
+//       await robotsArr[i].setProjects(projectsArr);
+//       await projectsArr[i].setRobots(robotsArr);
+//     }
+
+//     // seed your database here!
+//   } catch (err) {
+//     console.log(red(err));
+//   }
+// };
+
+// module.exports = seed
+// // If this module is being required from another module, then we just export the
+// // function, to be used as necessary. But it will run right away if the module
+// // is executed directly (e.g. `node seed.js` or `npm run seed`)
+// if (require.main === module) {
+//   seed()
+//     .then(() => {
+//       console.log(green('Seeding success!'))
+//       db.close()
+//     })
+//     .catch(err => {
+//       console.error(red('Oh noes! Something went wrong!'))
+//       console.error(err)
+//       db.close()
+//     })
+// }
