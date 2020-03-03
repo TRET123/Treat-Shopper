4a222c96d84090d64c15bba38dd2608b5e019c5b
Erick <ecanals07@gmail.com>
2020-03-02 21:50:40 -0500

    removed some comments from boilermaker to clean up code

--- seed.js
+++ seed.js
@@ -1,4 +1,3 @@
-// require database and User and Products model
 const Users = require('./server/db/models/user')
 const Products = require('./server/db/models/product')
 const Orders = require('./server/db/models/order')
@@ -8,12 +7,12 @@ const faker = require('faker')
 const instanceCount = 100
 const users = []
 
+// generating random users
 for (let i = 0; i < instanceCount; i++) {
   const randomUser = {
     email: faker.fake('{{internet.email}}'),
     password: faker.fake('{{internet.password}}'),
     address: faker.fake('{{address.streetAddress}}'),
-    admin: faker.fake('{{random.boolean}}'),
     lastName: faker.fake('{{name.lastName}}'),
     firstName: faker.fake('{{name.firstName}}')
   }
@@ -30,31 +29,13 @@ const seed = async () => {
         return Users.create(user)
       })
     )
-
-    // await Promise.all(
-    //   projects.map(project => {
-    //     return Project.create(project)
-    //   })
-    // )
-
-    // const robotsArr = await Robot.findAll()
-    // const projectsArr = await Project.findAll()
-
-    // for (let i = 0; i < robots.length; i++) {
-    //   await robotsArr[i].setProjects(projectsArr)
-    //   await projectsArr[i].setRobots(robotsArr)
-    // }
-
-    // seed your database here!
   } catch (err) {
     console.error(err)
   }
 }
 
 module.exports = seed
-// If this module is being required from another module, then we just export the
-// function, to be used as necessary. But it will run right away if the module
-// is executed directly (e.g. `node seed.js` or `npm run seed`)
+
 if (require.main === module) {
   seed()
     .then(() => {
@@ -62,7 +43,6 @@ if (require.main === module) {
       db.close()
     })
     .catch(err => {
-      console.error('Oh noes! Something went wrong!')
       console.error(err)
       db.close()
     })
--- server/db/models/product.js
+++ server/db/models/product.js
@@ -6,26 +6,32 @@ const Product = db.define('product', {
     type: Sequelize.STRING,
     allowNull: false
   },
+
   inventory: {
     type: Sequelize.INTEGER,
     allowNull: false
   },
+
   price: {
     type: Sequelize.FLOAT,
     allowNull: false
   },
+
   description: {
     type: Sequelize.TEXT,
     allowNull: false
   },
+
   candyType: {
     type: Sequelize.STRING,
     allowNull: false
   },
+
   calories: {
     type: Sequelize.INTEGER,
     allowNull: false
   },
+
   imageUrl: {
     type: Sequelize.STRING,
     allowNull: false
