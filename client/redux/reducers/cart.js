262a3672173abdd95f63b4353cf86bf6ae889d66
Erick <ecanals07@gmail.com>
2020-03-03 21:29:51 -0500

    edited redux for the new cart model

--- client/redux/reducers/cart.js
+++ client/redux/reducers/cart.js
@@ -1,13 +1,13 @@
-import {GET_CART_PRODUCTS} from '../action-types/cart'
+import {GET_CART} from '../action-types/cart'
 
 const initialState = {
-  cartProducts: []
+  cart: {}
 }
 
 export const cart = (state = initialState, action) => {
   switch (action.type) {
-    case GET_CART_PRODUCTS:
-      return {...state, cartProducts: action.cartProducts}
+    case GET_CART:
+      return {...state, cart: action.cart}
     default:
       return state
   }
--- client/redux/thunks/cart.js
+++ client/redux/thunks/cart.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
-import {getCartProducts} from '../action-creators/cart'
+import {getCart} from '../action-creators/cart'
 
-export const getCartProductsThunk = () => {
+export const getCartThunk = () => {
   return async dispatch => {
     try {
       const {data} = await axios.get('/api/cart')
-      dispatch(getCartProducts(data))
+      dispatch(getCart(data))
     } catch (error) {
       console.error(error)
     }
