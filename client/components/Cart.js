fe59065c9633cea220f04a5e5830ca5aa926b355
Erick <ecanals07@gmail.com>
2020-03-07 09:30:37 -0500

    rana fixed cart not rendering bug

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -125,9 +125,6 @@ class Cart extends Component {
     ) : (
       <p>Your cart is empty</p>
     )
-    // if (addedItems.length) {
-    //   return true
-    // }
     return (
       <div className="shopping-cart">
         <div>
