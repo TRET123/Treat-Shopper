24826ca2785f4640faf9046be8ea633f46dbeeae
Rana Quadri <rq59@ymail.com>
2020-03-08 14:13:43 -0400

    Able to edit email address in user profile

--- client/components/UserProfile.js
+++ client/components/UserProfile.js
@@ -1,13 +1,45 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {me} from '../redux/thunks/user'
+import {me, updateUser} from '../redux/thunks/user'
 
 class UserProfile extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      id: this.props.user.id,
+      firstName: this.props.user.firstName,
+      lastName: this.props.user.lastName,
+      email: this.props.user.email
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
   async componentDidMount() {
     await this.props.getUser()
   }
+  handleSubmit(evt) {
+    evt.preventDefault()
+    const user = {
+      id: this.state.id,
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: this.state.email
+    }
 
+    this.props.updateUser(user)
+  }
+
+  handleChange(evt) {
+    console.log('name in handleCh', evt.target)
+    this.setState({
+      [evt.target.name]: evt.target.value
+    })
+  }
   render() {
+    const isEnabled =
+      this.state.email.length > 0 &&
+      this.state.firstName.length > 0 &&
+      this.state.lastName.length > 0
     return (
       <div className="user-profile">
         <h5 className="title"> User Profile </h5>
@@ -16,10 +48,23 @@ class UserProfile extends Component {
           Name: {`${this.props.user.firstName} `}
           {this.props.user.lastName}
         </span>
-        <div className="user-profile-item">Email: {this.props.user.email}</div>
+        <div className="user-profile-item" defaultValue={this.props.user.email}>
+          Email: {this.state.email}
+        </div>
+
+        <input
+          placeholder={this.props.user.email}
+          name="name"
+          type="email"
+          value={this.state.email}
+          onChange={evt => this.setState({email: evt.target.value})}
+        />
         <div className="user-profile-item">
           Address: {this.props.user.address}
         </div>
+        <button disabled={!isEnabled} type="submit" onClick={this.handleSubmit}>
+          Update
+        </button>
       </div>
     )
   }
@@ -33,7 +78,9 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    getUser: () => dispatch(me())
+    getUser: () => dispatch(me()),
+    updateUser: (id, firstName, lastName, email) =>
+      dispatch(updateUser(id, firstName, lastName, email))
   }
 }
 
--- client/redux/action-creators/user.js
+++ client/redux/action-creators/user.js
@@ -1,4 +1,5 @@
-import {GET_USER, REMOVE_USER} from '../action-types/user'
+import {GET_USER, REMOVE_USER, UPDATE_USER} from '../action-types/user'
 
 export const getUser = user => ({type: GET_USER, user})
 export const removeUser = () => ({type: REMOVE_USER})
+export const updatedUser = () => ({type: UPDATE_USER})
