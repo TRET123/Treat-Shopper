b20645e3a104b032cd6d6ee0d1687d93258b5212
Thomas Zhang <thomaszhang97@gmail.com>
2020-03-09 14:05:38 -0400

    validation applied, but not working & submit button functionality working, but link tags need to be removed

--- server/auth/google.js
+++ server/auth/google.js
@@ -55,7 +55,7 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
   )
 
   router.get(
-    '/callback',
+    '/auth/google/verify',
     passport.authenticate('google', {
       successRedirect: '/home',
       failureRedirect: '/login'
